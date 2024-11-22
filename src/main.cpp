ecc3329c2cc2e3766f490ee2635b414313999779
Beat <75857504+BeatACVR@users.noreply.github.com>
2024-11-22 07:54:53 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,6 @@ public:
 
     auto clickSoundFile = Mod::get()->getSettingValue<std::filesystem::path>("custom-presssound").string();
 
-	  if (Mod::get()->getSettingValue<std::filesystem::path>("custom-presssound").string() == "Select a file") {
-      clickSoundFile = "default-click.ogg"_spr;
-    }
-
     // Play click sound
     FMODAudioEngine::sharedEngine()->playEffect(clickSoundFile);
     return ret;
@@ -50,10 +46,6 @@ public:
     log::debug("CSLite: Player jumped.");
 
     auto releaseSoundFile = Mod::get()->getSettingValue<std::filesystem::path>("custom-releasesound").string();
-
-    if (Mod::get()->getSettingValue<std::filesystem::path>("custom-releasesound").string() == "Select a file") {
-      releaseSoundFile = "default-release.ogg"_spr;
-    }
 
     // Play click sound
     FMODAudioEngine::sharedEngine()->playEffect(releaseSoundFile);
