187d8920db55793a2b257e18efcac0ff827c0529
Beat <75857504+BeatACVR@users.noreply.github.com>
2024-11-22 21:18:43 -0600

    fix indenting

--- src/main.cpp
+++ src/main.cpp
@@ -5,52 +5,54 @@
 using namespace geode::prelude;
 
 class $modify(PlayerObject) {
-
 public:
+    // click sounds
+    bool pushButton(PlayerButton p0) {
+        bool ret = PlayerObject::pushButton(p0);
 
-// Click sounds
-	bool pushButton(PlayerButton p0) {
-		bool ret = PlayerObject::pushButton(p0);
+        if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
+            if (p0 != PlayerButton::Jump) {
+                return ret;
+            }
+        }
 
-	  if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
-    	if (p0 != PlayerButton::Jump) {
-    		return ret;
-    	}
+        if (!GameManager::sharedState()->getPlayLayer()) {
+            return ret;
+        }
+
+        auto clickSoundFile = Mod::get()->getSettingValue<std::filesystem::path>("custom-presssound").string();
+        auto isClickEnabled = Mod::get()->getSettingValue<bool>("enable-clicksounds");
+
+        // play click sound
+        if (isClickEnabled) {
+            FMODAudioEngine::sharedEngine()->playEffect(clickSoundFile);
+        }
+
+        return ret;
     }
 
-    if (!GameManager::sharedState()->getPlayLayer())
-      return ret;
+    // release sounds
+    bool releaseButton(PlayerButton p0) {
+        bool ret = PlayerObject::releaseButton(p0);
 
-    auto clickSoundFile = Mod::get()->getSettingValue<std::filesystem::path>("custom-presssound").string();
-    auto isClickEnabled = Mod::get()->getSettingValue<bool>("enable-clicksounds");
+        if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
+            if (p0 != PlayerButton::Jump) {
+                return ret;
+            }
+        }
 
-    // Play click sound
-    if (isClickEnabled)
-        FMODAudioEngine::sharedEngine()->playEffect(clickSoundFile);
-    
-    return ret;
-  }
+        if (!GameManager::sharedState()->getPlayLayer()) {
+            return ret;
+        }
 
-// Release sounds
-	bool releaseButton(PlayerButton p0) {
-		bool ret = PlayerObject::releaseButton(p0);
+        auto releaseSoundFile = Mod::get()->getSettingValue<std::filesystem::path>("custom-releasesound").string();
+        auto isReleaseEnabled = Mod::get()->getSettingValue<bool>("enable-releasesounds");
 
-	  if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
-      if (p0 != PlayerButton::Jump) {
-      	return ret;
-      }
+        // play release sound
+        if (isReleaseEnabled) {
+            FMODAudioEngine::sharedEngine()->playEffect(releaseSoundFile);
+        }
+
+        return ret;
     }
-
-    if (!GameManager::sharedState()->getPlayLayer())
-      return ret;
-
-    auto releaseSoundFile = Mod::get()->getSettingValue<std::filesystem::path>("custom-releasesound").string();
-    auto isReleaseEnabled = Mod::get()->getSettingValue<bool>("enable-releasesounds");
-
-    // Play click sound
-    if (isReleaseEnabled)
-        FMODAudioEngine::sharedEngine()->playEffect(releaseSoundFile);
-    
-    return ret;
-  }
 };
