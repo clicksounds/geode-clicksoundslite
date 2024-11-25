182d1aaeff0c6581d41aa71dd2eaefafccc5d659
BeatACVR <75857504+BeatACVR@users.noreply.github.com>
2024-11-25 12:47:16 -0600

    1.0.4 settings button update

--- changelog.md
+++ changelog.md
@@ -4,3 +4,5 @@
 - Minor fix: .m4a files have been removed from file selection.
 ### 1.0.3
 - Minor fix: Click and release sound will now play in editor testing.
+### 1.0.4
+- Minor fix: Added the mod settings button to the pause screen
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayerObject.hpp>
-#include <Geode/binding/FMODAudioEngine.hpp>
+#include <Geode/modify/PauseLayer.hpp>
+#include <Geode/ui/GeodeUI.hpp>
 
 using namespace geode::prelude;
 
@@ -10,7 +11,7 @@ public:
     bool pushButton(PlayerButton p0) {
         bool ret = PlayerObject::pushButton(p0);
 
-        if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
+        if (Mod::get()->getSettingValue<bool>("only-on-jump")) {
             if (p0 != PlayerButton::Jump) {
                 return ret;
             }
@@ -36,12 +37,13 @@ public:
     bool releaseButton(PlayerButton p0) {
         bool ret = PlayerObject::releaseButton(p0);
 
-        if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
+        if (Mod::get()->getSettingValue<bool>("only-on-jump")) {
             if (p0 != PlayerButton::Jump) {
                 return ret;
             }
         }
 
+        // only continue if the player isnt in the editor or in gameplay
         if (!GameManager::sharedState()->getPlayLayer() && !GameManager::sharedState()->getEditorLayer()) {
             return ret;
         }
@@ -56,4 +58,30 @@ public:
 
         return ret;
     }
+};
+
+// Create settings button in the pause menu if enabled in settings
+class $modify(CSLitePauseLayer, PauseLayer) {
+  void openCSLiteSettings(CCObject*) {
+      geode::openSettingsPopup(Mod::get());
+  }
+
+  void customSetup() {
+    PauseLayer::customSetup();
+    auto menu = this->getChildByID("left-button-menu");
+    auto winSize = CCDirector::sharedDirector()->getWinSize();
+    auto spr = CCSprite::create("csLiteSettingsSprite.png"_spr);
+
+    auto btn = CCMenuItemSpriteExtra::create(
+        spr,
+        this,
+        menu_selector(CSLitePauseLayer::openCSLiteSettings)
+      );
+    spr->setScale(0.7f);
+    if(Mod::get()->getSettingValue<bool>("settings-button")) {
+      btn->setPosition({menu->getContentSize().width / 2, btn->getContentSize().height / 2});
+      btn->setID("csLiteSettingsButton");
+      menu->addChild(btn);
+    }
+  }
 };
